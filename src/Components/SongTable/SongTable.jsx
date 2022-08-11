475eb4b91365602ea90888efaa462dfea4a4a8d6
Devin Chen <taunwe79@gmail.com>
2022-08-11 04:41:25 -0500

    added functionality to be able to add a song to the library

--- src/Components/SongTable/SongTable.jsx
+++ src/Components/SongTable/SongTable.jsx
@@ -1,36 +1,56 @@
-import React from 'react';
-import './SongTable.css'
+import React, {Component} from 'react';
+import './SongCreator.css'
 
-const SongTable = (props) => {
-    return (
-        <div>
-            <h1 className="text-center">Song Table</h1>
-            <table className="table table-bordered">
-                <thead>
-                    <th scope="col">Title</th>
-                    <th scope="col">Artist</th>
-                    <th scope="col">Album</th>
-                    <th scope="col">Genre</th>
-                    <th scope="col">Release Date</th>
-                    <th className="text-center"scope="col">Delete</th>
-                </thead>
-                <tbody>
-                    {props.songs.map(song => {
-                        return (
-                            <tr>
-                                <td>{song.title}</td>
-                                <td>{song.artist}</td>
-                                <td>{song.album}</td>
-                                <td>{song.genre}</td>
-                                <td>{song.release_date}</td>
-                                <td className="text-center"><button type="button" className="btn btn-primary" onClick={()=> props.deleteSong(song.id)}>Delete</button></td>
-                            </tr>                          
-                        )
-                    })}
-                </tbody>    
-            </table>
-        </div>
-    )
+class SongCreator extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            title : '',
+            artist : '',
+            album : '',
+            genre : '',
+            release_date : '',
+        }
+    }
+
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addSong(this.state)
+        this.setState({
+            title : '',
+            artist : '',
+            album : '',
+            genre : '',
+            release_date : '',
+        })
+        
+    }
+    
+    render() {
+        return (
+            <div className="form-group">
+                <h1 className="text-center">Song Creator</h1>
+                <form className="form-control"onSubmit={(event)=> this.handleSubmit(event)}>
+                    Title: <input className="form-control" type="text" name="title"value={this.state.title}onChange={this.handleChange}/><br />
+                    Artist: <input className="form-control"type="text" name="artist"value={this.state.artist}onChange={this.handleChange}/><br />
+                    Album: <input className="form-control"type="text" name="album"value={this.state.album}onChange={this.handleChange}/><br />
+                    Genre: <input className="form-control"type="text" name="genre"value ={this.state.genre}onChange={this.handleChange}/><br />
+                    Release Date: <input className="form-control"type="date"name="release_date"value={this.state.release_date}onChange={this.handleChange}/>
+                    <br/>
+                    <div className="text-center"><button className="btn btn-primary"type="submit" value="submit">Submit</button></div>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default SongTable;
+export default SongCreator;
